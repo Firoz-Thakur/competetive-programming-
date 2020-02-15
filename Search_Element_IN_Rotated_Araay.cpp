91dd5483adf7bf87a505ee8c744b18d96280aea0
Firoz kumar <56572016+firozbaba@users.noreply.github.com>
2020-02-15 11:49:18 +0530

    Create Search_Element_IN_Rotated_Araay.cpp

---
+++ Search_Element_IN_Rotated_Araay.cpp
@@ -0,0 +1,23 @@
+Rahul had a sorted array of numbers from which he had to find a given number quickly. His friend by mistake rotated the array. 
+Now Rahul doesn't have time to sort the elements again. Help him to quickly find the given number from the rotated array.
+
+ex: 4,5,6,7,1,2,3rotated arry:
+ 
+hind:- binary search
+
+The first line contains N - the size of the array. the next N lines contain the numbers of the array.The next line contains the item to
+be searched.
+
+input:
+7
+4,5,6,7,1,2,3
+5
+
+output:
+
+
+
+
+
+
+
