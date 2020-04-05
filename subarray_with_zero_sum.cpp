c903c77700498a9826ab7f73718631e9f49329fa
Firoz kumar <56572016+Firoz-kumar-3@users.noreply.github.com>
2020-04-05 21:52:48 +0530

    Update subarray_with_zero_sum.cpp

--- subarray_with_zero_sum.cpp
+++ subarray_with_zero_sum.cpp
@@ -27,3 +27,32 @@ bool subArrayExists(int arr[], int n)
       } 
     return false;
 }
+
+
+//length of the largest subarray--->>
+
+int maxLen(int ar[],int n)
+{
+  //Your code here
+  map<int,int> mp;
+  int sum=0;
+  int len=0;
+  int flag;
+  for(int i=0;i<n;i++)
+  {   flag=0;
+      sum=sum+ar[i];
+     // cout<<sum<<" ";
+      if(sum==0)
+        len=i+1;  ///if the sum of the array become zero ,it will only possible form i=0 to i=j(any index)
+      if(mp.count(sum))
+      {
+               if(len<(i-(mp[sum])))
+                len=i-(mp[sum]);
+                flag=1;   
+      }
+      if(flag==0)    // if the 15 15 15 are comming three times in the array we will only contain the first index of the element so that 
+             mp[sum]=i;                 //we can get maximum subarray length
+     
+  }
+  return len;
+}
